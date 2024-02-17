bcc3e39f62e5735ccc55bbb5f1e8503d5bc39680
thassapongbot <158215972+thassapongbot@users.noreply.github.com>
2024-02-16 21:25:03 -0800

    Update allroutes.js

--- routes/allroutes.js
+++ routes/allroutes.js
@@ -6,7 +6,7 @@ let upload = multer();
 
 allRoutes.get('/', (req,res) => {
     console.log('Reaced root');
-    res.send("Welcome to FSD backend");
+    res.send("Welcome to automate git backend");
 });
 
 
